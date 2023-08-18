71a419256ba8ab02f776091feae103047bdd596a
Sampsa Heinonen <sampsavh@gmail.com>
2023-08-18 22:18:56 +0300

    set splash screen background color

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -15,6 +15,7 @@ import LaunchScreen from "./screens/LaunchScreen";
 import GambleScreen from './screens/GambleScreen';
 import SetupScreen from './screens/SetupScreen';
 import SavedGamesScreen from './screens/SavedGamesScreen';
+import BackgroundColor from 'react-native-background-color';
 
 import COLORS from './colors';
 const Stack = createNativeStackNavigator();
@@ -29,11 +30,20 @@ const theme = {
   },
 };
 
+const noAnimationTransition = {
+  gestureEnabled: false,
+  animation: 'none',
+};
+
 function App(): JSX.Element {
+
+  useEffect(() => {
+    BackgroundColor.setColor(COLORS.primary);
+  }, [])
 
   return (
     <NavigationContainer theme={theme}>
-      <Stack.Navigator initialRouteName='LaunchScreen'>
+      <Stack.Navigator initialRouteName='LaunchScreen' screenOptions={{animation: "slide_from_bottom"}}>
         <Stack.Screen name="LaunchScreen" component={LaunchScreen} options={{headerShown: false}}/>
         <Stack.Screen name="GambleScreen" component={GambleScreen} options={{headerShown: false}}/>
         <Stack.Screen name="SetupScreen" component={SetupScreen} options={{headerShown: false}}/>
